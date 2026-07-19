1bf2045d78dfb7627a62d2fe991aeb0bb9bd9c6b
Paweł Wideł <pawel.widel@gmail.com>
2026-07-19 11:35:56 +0200

    Complete Case Study #2: Best Selling Products

--- 01_sales_performance/02_best_selling_products.sql
+++ 01_sales_performance/02_best_selling_products.sql
@@ -1,3 +1,12 @@
+/*Business Scenario
+
+The Sales Director wants to identify which products generate the highest revenue in order to support pricing, marketing, and inventory decisions.
+
+Business Question
+
+Which products generated the highest total sales revenue?
+*/
+
 select production.Product.Name, SUM(sales.SalesOrderDetail.LineTotal) AS TotalRevenue FROM Production.Product
 INNER JOIN sales.SalesOrderDetail 
 ON production.Product.productid = sales.SalesOrderDetail.productid
